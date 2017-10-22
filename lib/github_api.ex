098c99a8069a5ae341023f52fa0c7a61619cda6d
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2017-10-22 09:08:12 +0300

    initial

---
+++ lib/github_api.ex
@@ -0,0 +1,18 @@
+defmodule GithubApi do
+  @moduledoc """
+  Documentation for GithubApi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GithubApi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/github_api_test.exs
@@ -0,0 +1,8 @@
+defmodule GithubApiTest do
+  use ExUnit.Case
+  doctest GithubApi
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
